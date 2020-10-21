c906143c11c5c42efdfc55f7b51fb4b98bdb1fed
Ivange  Larry <ivangelarry@gmail.com>
2020-10-21 14:19:41 +0100

    Add utils.parse and failing test

---
+++ src/utils.test.js
@@ -0,0 +1,20 @@
+import { parse } from './utils';
+
+const mockResult = [{
+        name: "New York",
+        postalCode: 2005
+    },
+    {
+        name: "Tokyo",
+        postalCode: 3006
+    },
+    {
+        name: "Florida",
+        postalCode: 2008
+    }
+]
+
+test('Parse string to location objects', () => {
+    const value = "New York, 2005, Tokyo, 3006, Florida, 2008";
+    expect(parse(value)).toEqual(mockResult);
+})
