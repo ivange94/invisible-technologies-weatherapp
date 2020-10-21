a0882c1c4684397d2b2699a317c994eadb9c29c5
Ivange  Larry <ivangelarry@gmail.com>
2020-10-21 14:27:05 +0100

    Add working implementation of utils.parse

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
 export const parse = value => {
-
+    const values = value.split(',')
+    const locationObjects = []
+    values.forEach((tmpVal, index) => {
+        const location = {};
+        if (index % 2 === 1) {
+            location.name = values[index - 1].trim()
+            location.postalCode = parseInt((tmpVal.trim()))
+            locationObjects.push(location)
+        }
+    })
+    return locationObjects
 }
